3c81f499276e4560a59d366280d57188fcc4a7bf
Documenter.jl <documenter@juliadocs.github.io>
2022-06-01 14:56:58 +0000

    build based on ae631d5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SphericalGraphEmbedding","category":"page"},{"location":"#SphericalGraphEmbedding","page":"Home","title":"SphericalGraphEmbedding","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SphericalGraphEmbedding.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SphericalGraphEmbedding]","category":"page"}]
+}
